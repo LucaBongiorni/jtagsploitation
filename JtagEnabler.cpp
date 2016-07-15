fd86f013408c4e8ed00393f52ede8942704aa015
Piotr Esden-Tempski <piotr@esden.net>
2016-07-15 15:32:20 -0700

    Fixed a cut and paste error.

--- JtagEnabler.cpp
+++ JtagEnabler.cpp
@@ -67,26 +67,6 @@ int main()
         }
 
         selector.SetGPIOFunction(22, GPIO_ALT_FUNCTION_4);
-                unsigned oldValue = m_pAltFunctionRegisters[registerIndex];
-                unsigned mask = 0b111 << bit;
-                printf("Changing function of GPIO%d from %x to %x\n", GPIO, (oldValue >> bit) & 0b111, functionCode);
-                m_pAltFunctionRegisters[registerIndex] = (oldValue & ~mask) | ((functionCode << bit) & mask);
-        }
-};
-
-#define GPIO_ALT_FUNCTION_4 0b011
-#define GPIO_ALT_FUNCTION_5 0b010
-
-int main()
-{
-        GpioFunctionSelector selector;
-        if (!selector.IsValid())
-        {
-                printf("Cannot open /dev/mem! Re-run me as root!\n");
-                return 1;
-        }
-
-        selector.SetGPIOFunction(22, GPIO_ALT_FUNCTION_4);
         selector.SetGPIOFunction(4,  GPIO_ALT_FUNCTION_5);
         selector.SetGPIOFunction(27, GPIO_ALT_FUNCTION_4);
         selector.SetGPIOFunction(25, GPIO_ALT_FUNCTION_4);
